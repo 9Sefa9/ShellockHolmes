de369259740b49d191a13a34bd039aa34e4fc39f
9Sefa9 <sefagoevercin@hotmail.de>
2021-04-07 20:04:40 +0200

    a commit message

--- gitPuller.sh
+++ gitPuller.sh
@@ -5,7 +5,7 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
 echo "ENTERING LINUX STATEMENT"
 for d in */; do 	
 	if [ -d "$d.git" ]; then
-                echo "PULLING EXISTING GIT PATH:: $d"
+                echo "################### PULLING EXISTING GIT PATH:: $d ###################"
  		cd "$d";git pull; cd ..
     fi
     done
@@ -15,7 +15,7 @@ else
 	for d in */; do 
 		
 		if [ -d "$d\.git" ]; then
-        	        echo "PULLING EXISTING GIT PATH:: $d"
+        	        echo "################### PULLING EXISTING GIT PATH:: $d ###################"
  			cd "$d";git pull ; cd ..
     	fi
     done
